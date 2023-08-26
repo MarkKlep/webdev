50c4ed9a28ff09ad02b81eb5ce651aa9d3f4c45e
mark <cheapshoterr@gmail.com>
2023-08-26 21:08:19 +0300

    inp text

--- script.js
+++ script.js
@@ -4,8 +4,19 @@ const sliderLine = document.querySelector('.slider-line');
 
 document.querySelector('.slider-next').onclick = () => {
     offset += 256;
-    if(offset > 265*3) {
+    if(offset > 768) {
         offset = 0;
     }
-    sliderLine.style.right = offset + 'px'; 
+    sliderLine.style.right = offset + 'px';
+}
+
+//========================INPout====================================
+
+
+document.querySelector('.b-1').onclick = () => {
+    const data = document.querySelector('.inp-1').value;
+    if(data.trim().length) {
+        document.querySelector(".out-1").textContent = data;
+        document.querySelector('.inp-1').value='';
+    }
 }
