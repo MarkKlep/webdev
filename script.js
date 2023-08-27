4798a9b158944ff6ace154bc15e7c57c061a03fa
mark <cheapshoterr@gmail.com>
2023-08-27 16:35:15 +0300

    inp DATE

--- script.js
+++ script.js
@@ -22,9 +22,21 @@ document.querySelector('.b-1').onclick = () => {
 }
 
 document.querySelector('.b-2').addEventListener('click', ()=>{
-    const data = document.querySelector(".inp-2").value;
-    if(data){
+    const data = +document.querySelector(".inp-2").value; //number not string due to +
+    console.log(data + 10);
+    if(data) {
         document.querySelector(".out-2").textContent = data;
         document.querySelector(".inp-2").value = '';
     }
+})
+
+document.querySelector('.b-3').addEventListener('click', ()=>{
+    const data = document.querySelector('.inp-3').value;
+    document.querySelector('.out-3').textContent = data;
+})
+
+document.querySelector('.b-4').addEventListener('click', ()=>{
+    const data = document.querySelector('.inp-4').value;
+    if(data)
+        document.querySelector('.out-4').innerText = data;
 })
